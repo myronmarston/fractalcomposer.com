a31430e8b1ee0390926d99cf32361cbb9459e932
Myron Marston <myron.marston@gmail.com>
2008-05-06 05:41:01 +0000

    Checking in rails app structure.
    git-svn-id: file:///repos/FractalComposerDotCom/trunk@2 630928a3-496c-f744-9797-d1d2669399c2

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '6d5f829862d5cd3b9c825f3793d6b7d4'
+end
