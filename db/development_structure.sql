c3f18f13033306c09d60dda936ebd07edd591025
Myron Marston <myron.marston@gmail.com>
2008-09-14 01:25:13 +0000

    On IE/Chrome, live validation, 'null' was being printed, so changed null to the empty string.
    git-svn-id: file:///repos/FractalComposerDotCom/trunk@121 630928a3-496c-f744-9797-d1d2669399c2

--- db/development_structure.sql
+++ db/development_structure.sql
@@ -1,36 +1,66 @@
-CREATE TABLE `generated_pieces` (
+CREATE TABLE `comments` (
   `id` int(11) NOT NULL auto_increment,
-  `user_ip_address` varchar(255) collate utf8_bin NOT NULL,
-  `fractal_piece` text collate utf8_bin NOT NULL,
-  `generated_midi_file` varchar(255) collate utf8_bin NOT NULL,
+  `comment` text collate utf8_bin NOT NULL,
+  `commentable_id` int(10) NOT NULL,
+  `commentable_type` varchar(50) collate utf8_bin NOT NULL,
+  `ip_address` varchar(255) collate utf8_bin NOT NULL,
+  `name` varchar(80) collate utf8_bin NOT NULL,
+  `email` varchar(255) collate utf8_bin NOT NULL,
+  `website` varchar(255) collate utf8_bin NOT NULL default '',
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-CREATE TABLE `page_html_parts` (
+CREATE TABLE `generated_pieces` (
   `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) collate utf8_bin NOT NULL default '',
-  `content` text collate utf8_bin,
-  `content_html` text collate utf8_bin,
+  `user_ip_address` varchar(255) collate utf8_bin NOT NULL,
+  `fractal_piece` mediumtext collate utf8_bin,
+  `generated_midi_file` varchar(255) collate utf8_bin NOT NULL,
+  `generated_guido_file` varchar(255) collate utf8_bin NOT NULL,
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `ip_addresses` (
+  `id` int(11) NOT NULL auto_increment,
+  `ip_address` varchar(20) collate utf8_bin NOT NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `index_ip_addresses_on_ip_address` (`ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `page_html_parts` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) collate utf8_bin NOT NULL,
+  `content` mediumtext collate utf8_bin,
+  `content_html` mediumtext collate utf8_bin,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL auto_increment,
+  `rater_id` int(10) default NULL,
+  `rated_id` int(10) default NULL,
+  `rated_type` varchar(255) collate utf8_bin default NULL,
+  `rating` decimal(10,0) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `index_ratings_on_rater_id` (`rater_id`),
+  KEY `index_ratings_on_rated_type_and_rated_id` (`rated_type`,`rated_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `schema_info` (
   `version` int(10) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `schema_migrations` (
-  `version` varchar(255) collate utf8_bin NOT NULL default '',
-  UNIQUE KEY `unique_schema_migrations` (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 CREATE TABLE `sessions` (
   `id` int(11) NOT NULL auto_increment,
-  `session_id` varchar(255) collate utf8_bin NOT NULL default '',
-  `data` text collate utf8_bin,
+  `session_id` varchar(255) collate utf8_bin NOT NULL,
+  `data` mediumtext collate utf8_bin,
   `created_at` datetime default NULL,
   `updated_at` datetime default NULL,
   PRIMARY KEY  (`id`),
@@ -38,4 +68,45 @@ CREATE TABLE `sessions` (
   KEY `index_sessions_on_updated_at` (`updated_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `schema_info` (version) VALUES (3)
+CREATE TABLE `user_submission_unique_page_views` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_submission_id` int(10) NOT NULL,
+  `ip_address` varchar(20) collate utf8_bin NOT NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `fk_user_submission_unique_page_views` (`user_submission_id`),
+  CONSTRAINT `fk_user_submission_unique_page_views` FOREIGN KEY (`user_submission_id`) REFERENCES `user_submissions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `user_submissions` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) collate utf8_bin NOT NULL,
+  `email` varchar(255) collate utf8_bin NOT NULL,
+  `display_email` tinyint(1) NOT NULL default '0',
+  `comment_notification` tinyint(1) NOT NULL default '1',
+  `website` varchar(255) collate utf8_bin default NULL,
+  `title` varchar(255) collate utf8_bin NOT NULL,
+  `description` varchar(255) collate utf8_bin default NULL,
+  `generated_piece_id` int(10) NOT NULL,
+  `processing_began` datetime default NULL,
+  `processing_completed` datetime default NULL,
+  `piece_mp3_file` varchar(255) collate utf8_bin default NULL,
+  `piece_pdf_file` varchar(255) collate utf8_bin default NULL,
+  `piece_image_file` varchar(255) collate utf8_bin default NULL,
+  `germ_mp3_file` varchar(255) collate utf8_bin default NULL,
+  `germ_image_file` varchar(255) collate utf8_bin default NULL,
+  `comment_count` int(10) NOT NULL default '0',
+  `total_page_views` int(10) NOT NULL default '0',
+  `unique_page_views` int(10) NOT NULL default '0',
+  `rating_count` int(10) default NULL,
+  `rating_total` decimal(10,0) default NULL,
+  `rating_avg` decimal(10,2) default NULL,
+  `created_at` datetime default NULL,
+  `updated_at` datetime default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `fk_user_submissions_generated_piece_id_to_generated_pieces` (`generated_piece_id`),
+  CONSTRAINT `fk_user_submissions_generated_piece_id_to_generated_pieces` FOREIGN KEY (`generated_piece_id`) REFERENCES `generated_pieces` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `schema_info` (version) VALUES (6)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,7 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 function create_live_validation_field_basic(id_or_element, delay, validations) {    
-  var validator = new LiveValidation(id_or_element, {validMessage: null, wait: delay});
+  var validator = new LiveValidation(id_or_element, {validMessage: '', wait: delay});
   
   validations.each (function(validation) {
     validator.add(validation.type, validation.args);    
